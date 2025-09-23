33d73258d3228b65dec3bd67a9b0dfddcaba0ed3
Baitos <ryanisawesome1991@gmail.com>
2025-09-22 21:58:40 -0400

    wow its organized now

---
+++ headers/createTiles.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#include <SDL3/SDL.h> // don't include SDL3_main in anything other than the main cpp file
+#include <SDL3_image/SDL_image.h>
+#include <vector>
+#include <string>
+#include <array>
+#include <iostream>
+#include <format>
+
+#include "init.h"
+#include "gameobject.h"
+
+void createTiles(const SDLState &state, GameState &gs, const Resources &res);
---
+++ headers/draw.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdio.h>
+#include <SDL3/SDL.h> // don't include SDL3_main in anything other than the main cpp file
+#include <SDL3_image/SDL_image.h>
+#include <vector>
+#include <string>
+#include <array>
+#include <iostream>
+#include <format>
+
+#include "init.h"
+#include "gameobject.h"
+
+
+void drawParallaxBackground(SDL_Renderer *renderer, SDL_Texture *texture, float xVelocity, float &scrollPos, float scrollFactor, float deltaTime);
+void drawObject(const SDLState &state, GameState &gs, GameObject &obj, float width, float height, float deltaTime);
