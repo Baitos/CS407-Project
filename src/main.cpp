cb56633f236f64e641a4c2a66d46e4b416d20681
EHohmann1 <112178303+EHohmann1@users.noreply.github.com>
2025-10-27 14:18:22 -0400

    Added ENET library and modified main to act like client

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#define ENET_IMPLEMENTATION
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -23,10 +24,40 @@
 #include "../headers/playerState.h"
 #include "../headers/menu.h"
 
+#include "../headers/enet.h"
+
+//Add as needed
+struct PlayerUpdate{
+    int playerID;
+    float x;
+    float y;
+    float velX;
+    float velY;
+    PlayerState state;
+};
+
+//Globals for Game
 GameState * currState;
 using namespace std;
 
+//Globals for Networking
+uint64_t lastNetUpdate = 0;
+const float netTick = 1.0f / 20.0f; //20 Updates per second
+ENetPeer* serverPeer = nullptr;
+int pendingLobby = -1;
+
 int main(int argc, char** argv) { // SDL needs to hijack main to do stuff; include argv/argc
+    
+    //Initialize the ENET Library
+    if (enet_initialize() != 0) {
+        std::cout << "ENet initialization failed\n";
+        return 1;
+    }
+
+    //Deinitialize Library when you quit
+    atexit(enet_deinitialize);
+
+    //Create Window
     SDLState state;
     state.width = 1600;
     state.height = 900;
@@ -36,36 +67,18 @@ int main(int argc, char** argv) { // SDL needs to hijack main to do stuff; inclu
     if (!initialize(state)) {
         return 1;
     }
+    
     // load game assets
     Resources res;
     res.load(state);
-    
 
     //Initial Game State
-    //CHANGE if testing a different screen and you want it up on start
-
-    // currState = new SettingsState();
-    // currState->currStateVal =  SETTINGS;
-    // currState->nextStateVal = CHAR_SELECT;
-    // currState->init = initSettings;
-    // currState->update = settingsUpdate;
-    // currState->render = drawSettings;
-    // currState->input = settingsInputs;
-    
-    currState = new CharSelectState();
-    currState->currStateVal =  CHAR_SELECT;
+    currState = new LevelState();
     currState->nextStateVal = SPACESHIP;
-    currState->init = initCharSelect;
-    currState->update = charSelectUpdate;
-    currState->render = drawCharSelect;
-    currState->input = charSelectInputs;
-    
-    // currState = new LevelState();
-    // currState->nextStateVal = SPACESHIP;
-    // currState->init = createTiles;
-    // currState->update = levelUpdate;
-    // currState->render = drawLevel;
-    // currState->input = levelInputs;
+    currState->init = createTiles;
+    currState->update = levelUpdate;
+    currState->render = drawLevel;
+    currState->input = levelInputs;
 
     // setup game data
     GameData gd(state);
@@ -76,9 +89,45 @@ int main(int argc, char** argv) { // SDL needs to hijack main to do stuff; inclu
     uint64_t FPS = 0;
     uint64_t lastTime = 0;
 
+
+    //Create your ENET Client
+    //ONLY NEEDS TO BE DONE WHEN JOINING/CREATING
+    ENetAddress clientAddress;
+    enet_address_set_host(&clientAddress, "100.74.129.69");
+    clientAddress.port = 0; // OS chooses port
+    ENetHost* client = enet_host_create(&clientAddress, 1, 2, 0, 0);
+    if (!client) {
+        printf("Bad client creation\n");
+        return -1;
+    }
+
+    //Set your host values, including IP address and port
+    ENetAddress address;
+    //IP Address changes per person
+    enet_address_set_host(&address, "100.76.236.38");
+    address.port = 1233;
+
+    //Attempt connection to the server
+    serverPeer = enet_host_connect(client, &address, 2, 0);
+    if (!serverPeer) {
+        printf("Bad connection\n");
+        return -1;
+    }
     
+    ENetEvent event;
+
+    while (enet_host_service(client, &event, 100) > 0) {
+            std::string message;
+            switch (event.type) {
+                case ENET_EVENT_TYPE_CONNECT: {   //Do the things you do when you connect
+                    printf("CONNECTED\n");
+                    break;
+                }
+            }
+        }
 
     // start game loop
+    
     while (running) {
         uint64_t nowTime = SDL_GetTicks(); // take time from previous frame to calculate delta
         frames++;
@@ -88,6 +137,23 @@ int main(int argc, char** argv) { // SDL needs to hijack main to do stuff; inclu
             frames = 0;
         }
         float deltaTime = (nowTime - prevTime) / 1000.0f; // convert to seconds from ms
+
+        while (enet_host_service(client, &event, 0) > 0) {
+            std::string message;
+            switch (event.type) {
+                case ENET_EVENT_TYPE_RECEIVE: {   //Add conditionals to do what server says
+                    //Draw appropriate info of packets given
+                    std::string message((char *) event.packet->data, event.packet->dataLength);
+                    enet_packet_destroy(event.packet);
+                    //gd.players_[]
+                    break;
+                }
+                case ENET_EVENT_TYPE_DISCONNECT: {  //Do the things to do when disconnecting
+                  
+                    break;
+                }
+            }
+        }
 
         //Calls functions related to the current GameState
         currState->input(state, gd, res, deltaTime);
@@ -104,11 +170,27 @@ int main(int argc, char** argv) { // SDL needs to hijack main to do stuff; inclu
         //swap buffers and present
         SDL_RenderPresent(state.renderer);
         prevTime = nowTime;
+
+        if ((nowTime - lastNetUpdate) / 1000.0f >= netTick) {
+            lastNetUpdate = nowTime;
+            PlayerUpdate playerInfo = {gd.playerIndex, gd.players_[gd.playerIndex].pos.x, gd.players_[gd.playerIndex].pos.y, gd.players_[gd.playerIndex].vel.x, gd.players_[gd.playerIndex].vel.y};
+
+            //SEND PACKET HERE
+            //std::string updateMsg = std::format(playerInfo);
+
+            ENetPacket* packet = enet_packet_create(&playerInfo,
+                                                    sizeof(playerInfo),
+                                                    ENET_PACKET_FLAG_UNSEQUENCED);
+
+            enet_peer_send(serverPeer, 0, packet);
+            enet_host_flush(client); // make sure packet is sent
+        }
         
     }
 
     delete currState;
     res.unload();
     cleanup(state);
+    enet_host_destroy(client);
     return 0;
 }
